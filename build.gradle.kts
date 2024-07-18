77e3ca97e2dd32aaab0b8f0f45eac305dfd0a9f0
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2024-07-18 16:41:43 -0400

    paper plugin and cleaning things up for myself

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("io.github.goooler.shadow") version "8.1.8" // Change back once goooler's PR goes through https://github.com/johnrengelman/shadow/pull/876
+    id("java")
+}
+
+group = "xyz.prorickey"
+version = "0.9.2"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.skriptlang.org/releases")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    implementation("com.github.SkriptLang:Skript:2.9.0")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    shadowJar {
+        destinationDirectory.set(file("${rootDir}/output"))
+        archiveClassifier.set("")
+        archiveBaseName.set("skUtilities")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
